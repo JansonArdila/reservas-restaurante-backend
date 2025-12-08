8e672e1c7b47ae4e0269b9c60a06aa4cf51d3748
Janson Ardila <yeison149@hotmail.com>
2025-12-08 15:11:40 -0500

    Dominio de email onboarding@resend.dev

--- utils/emailService.js
+++ utils/emailService.js
@@ -6,7 +6,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 async function sendMail(to, subject, html) {
     try {
         const response = await resend.emails.send({
-            from: "Reservas Restaurante <yeison149@hotmail.com>",
+            from: "Reservas Restaurante <onboarding@resend.dev>",
             to,
             subject,
             html
