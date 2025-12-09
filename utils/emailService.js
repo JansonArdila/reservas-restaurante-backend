60ea7fe255a8db49686615b3a1c7f22b12b460ae
Janson Ardila <yeison149@hotmail.com>
2025-12-08 19:29:55 -0500

    Actualización de brevo

--- utils/emailService.js
+++ utils/emailService.js
@@ -1,19 +1,29 @@
 const BrevoApi = require('@getbrevo/brevo');
 
 // Configura la API Key de Brevo
-BrevoApi.ApiClient.instance.authentications['api-key'].apiKey = process.env.BREVO_API_KEY;
+const apiKey = process.env.BREVO_API_KEY;
+if (!apiKey) {
+    throw new Error('La API Key de Brevo no está definida en el archivo .env');
+}
+
+// Crear una instancia de la API con la autenticación correcta
+const apiInstance = new BrevoApi.TransactionalEmailsApi();
+apiInstance.setApiKey('api-key', apiKey);  // Configura la clave API
 
 // Función para enviar el correo
 async function sendMail(to, subject, html) {
     try {
-        const apiInstance = new BrevoApi.TransactionalEmailsApi();
-        const sender = { email: 'juice.aestheticscol@gmail.com', name: 'Prueba PRESIK' };
+        // Configurar el remitente
+        const sender = {
+            email: 'juice.aestheticscol@gmail.com',  // Asegúrate de que este correo esté verificado
+            name: 'Prueba PRESIK'
+        };
 
         const sendSmtpEmail = new BrevoApi.SendSmtpEmail({
-            sender: sender,// El remitente
-            to: [{ email: to }], // El destinatario
-            subject: subject, // El asunto
-            htmlContent: html, // El contenido HTML
+            sender: sender,  // El remitente
+            to: [{ email: to }],  // El destinatario
+            subject: subject,  // El asunto
+            htmlContent: html,  // El contenido HTML
         });
 
         console.log("Enviando un correo con los siguientes datos", sendSmtpEmail);
@@ -29,3 +39,4 @@ async function sendMail(to, subject, html) {
 }
 
 module.exports = { sendMail };
+
